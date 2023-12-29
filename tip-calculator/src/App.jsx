3a37a3399d7a89e481aa0b0eb5fd496b8b47fab6
bhanu prakash <r19cs056@cit.reva.edu.in>
2023-12-29 07:20:40 +0530

    add controlled components

--- tip-calculator/src/App.jsx
+++ tip-calculator/src/App.jsx
@@ -1,32 +1,54 @@
 /* eslint-disable react/prop-types */
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [bill, setBill] = useState(0);
+  const [tip, setTip] = useState("0");
+  const [friendTip, setFriendTip] = useState("0");
+  const finalTip =
+    ((bill * Number(tip)) / 100 + (bill * Number(friendTip)) / 100) / 2;
   return (
     <div>
-      <Questions />
+      <Questions
+        bill={bill}
+        tip={tip}
+        friendTip={friendTip}
+        setBill={setBill}
+        setTip={setTip}
+        setFriendTip={setFriendTip}
+      />
+      {bill !== 0 ? <p>Tip is calculated here: {finalTip}</p> : null}
+      <Reset setBill={setBill} setTip={setTip} setFriendTip={setFriendTip} />
     </div>
   );
 }
 
-function Questions() {
+function Questions({ bill, tip, friendTip, setBill, setTip, setFriendTip }) {
   return (
     <div>
       <Question question="How was the bill?">
-        <input type="text" />
+        <input
+          type="number"
+          value={bill}
+          onChange={(e) => setBill(e.target.value)}
+        />
       </Question>
       <Question question="How did you like the service ?">
-        <select>
-          <option>Did not like it(0%)</option>
-          <option>It is good(10%)</option>
-          <option>It is great (20%)</option>
+        <select value={tip} onChange={(e) => setTip(e.target.value)}>
+          <option value="0">Did not like it(0%)</option>
+          <option value="10">It is good(10%)</option>
+          <option value="20">It is great (20%)</option>
         </select>
       </Question>
       <Question question="How did your friend like the service?">
-        <select>
-          <option>Did not like it(0%)</option>
-          <option>It is good(10%)</option>
-          <option>It is great (20%)</option>
+        <select
+          value={friendTip}
+          onChange={(e) => setFriendTip(e.target.value)}
+        >
+          <option value="0">Did not like it(0%)</option>
+          <option value="10">It is good(10%)</option>
+          <option value="20">It is great (20%)</option>
         </select>
       </Question>
     </div>
@@ -39,5 +61,14 @@ function Question({ question, children }) {
       {children}
     </div>
   );
+}
+
+function Reset({ setBill, setTip, setFriendTip }) {
+  function defaultValues() {
+    setBill(0);
+    setTip("0");
+    setFriendTip("0");
+  }
+  return <button onClick={defaultValues}>Reset</button>;
 }
 export default App;
