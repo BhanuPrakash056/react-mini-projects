c4bbea8657903842864dfe0906281608404f4c7d
bhanu prakash <r19cs056@cit.reva.edu.in>
2023-12-29 06:50:37 +0530

    add static component

--- tip-calculator/src/App.jsx
+++ tip-calculator/src/App.jsx
@@ -1,13 +1,43 @@
-
-import './App.css'
+/* eslint-disable react/prop-types */
+import "./App.css";
 
 function App() {
-
   return (
-    <>
-      
-    </>
-  )
+    <div>
+      <Questions />
+    </div>
+  );
 }
 
-export default App
+function Questions() {
+  return (
+    <div>
+      <Question question="How was the bill?">
+        <input type="text" />
+      </Question>
+      <Question question="How did you like the service ?">
+        <select>
+          <option>Did not like it(0%)</option>
+          <option>It is good(10%)</option>
+          <option>It is great (20%)</option>
+        </select>
+      </Question>
+      <Question question="How did your friend like the service?">
+        <select>
+          <option>Did not like it(0%)</option>
+          <option>It is good(10%)</option>
+          <option>It is great (20%)</option>
+        </select>
+      </Question>
+    </div>
+  );
+}
+function Question({ question, children }) {
+  return (
+    <div>
+      <span>{question}</span>
+      {children}
+    </div>
+  );
+}
+export default App;
